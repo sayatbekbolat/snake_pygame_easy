c18aa25932695238c99fe8e94b576258483bff52
sayatbekbolat <bekbolat.sayat@gmail.com>
2021-03-02 20:21:52 +0600

    Add game over conditions

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ clock = pygame.time.Clock()
 score = 0
 
 def message(msg, color):
-    mesg = pygame.font.render(msg, True, color)
-    dis.blit = (mesg, [dis_width/2, dis_height/2])
-    
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
@@ -36,23 +36,30 @@ while not game_over:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
+                x1_change = -snake_block
                 y1_change = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
+                x1_change = snake_block
                 y1_change = 0
             elif event.key == pygame.K_UP:
                 x1_change = 0
-                y1_change = -10
+                y1_change = -snake_block
             elif event.key == pygame.K_DOWN:
                 x1_change = 0
-                y1_change = 10
-        
+                y1_change = snake_block
+
+        if x1>dis_width or x1<0 or y1>dis_height or y1<0:
+            game_over = True
     x1 += x1_change
     y1 += y1_change
     dis.fill(WHITE)
-    pygame.draw.rect(dis, BLACK, (x1,y1,10,10))
+    pygame.draw.rect(dis, BLACK, (x1,y1,snake_block,snake_block))
     pygame.display.update()    
-    clock.tick(30)
+    clock.tick(snake_speed)
+
+message('You lost - GAME OVER', RED)
+pygame.display.update()
+time.sleep(2)
+
 pygame.quit()
 quit()
