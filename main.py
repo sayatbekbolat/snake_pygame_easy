4e503143d56cd402a80dd285b90fd0e6c5a4930b
sayatbekbolat <bekbolat.sayat@gmail.com>
2021-03-02 19:36:31 +0600

    Add snake unit commit

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import pygame
 import time
 
 pygame.init()
-
 dis = pygame.display.set_mode((400,300))
-pygame.display.update()
+
 pygame.display.set_caption('Snake!')
+
+BLUE = (0,0,255)
+RED = (255,0,0)
 
 game_over = False
 while not game_over:
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.QUIT:
+            game_over = True
+    pygame.draw.rect(dis, BLUE, (200,150,10,10))
+    pygame.display.update()    
 
 pygame.quit()
 quit()
