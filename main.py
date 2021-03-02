73d053e4309a101e423cd217bc5e2b317fc65293
sayatbekbolat <bekbolat.sayat@gmail.com>
2021-03-02 19:31:00 +0600

    Add event catch commit

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ pygame.init()
 
 dis = pygame.display.set_mode((400,300))
 pygame.display.update()
-time.sleep(5)
-print('asd')
+pygame.display.set_caption('Snake!')
+
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+
 pygame.quit()
 quit()
